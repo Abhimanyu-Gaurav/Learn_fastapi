fe2d31c85bc8129d35f167f85a2de308e6307cc0
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-09-10 11:55:24 +0530

    Add blog, unpublished, global id, comments

--- main.py
+++ main.py
@@ -4,17 +4,42 @@ from typing import Optional
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {"Hello! World"}
+# @app.get("/")
+# def index():
+#     return {"Hello! World"}
 
 
-@app.get("/about")
-def about():
-    return {"data" : "Welcome to FastAPI"}
+# @app.get("/about")
+# def about():
+#     return {"data" : "Welcome to FastAPI"}
 
 
-@app.get("/")
-def blogs():
-    return {"data" : "blog list"}
+# @app.get("/")
+# def blogs():
+#     return {"data" : "blog list"}
 
+@app.get("/blog")
+def index(limit=10, published: bool = True, sort: Optional[str]= None):
+    # only get 10 published blog or whatever we set.
+    if published:
+        return {"data" : f'{limit} published blogs from the db'}
+    else:
+        return {"data" : f'{limit} blogs from the db'}
+
+
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data" : "all unpublished blogs"}
+
+
+@app.get("/blog/{id}") # when we need dynamic routing then we put {id} in it.
+def show(id : int):
+    # fetch blog with id = id
+    return {"data" : id}
+
+
+@app.get("/blog/{id}/comments")
+    # fetch comments of blog with id = id
+def comments(id):
+    return {"data" : {'1','2'}}
