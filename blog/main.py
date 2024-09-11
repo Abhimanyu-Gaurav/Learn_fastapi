15bf3bc78dc8bfa83e4cd956ac22b8fe2fe46c96
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-09-11 11:46:28 +0530

    Pydantic schemas

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,17 @@
  ## To perform CURD operation
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Blog(BaseModel):
+    title : str
+    body : str
+
+
 @app.post('/blog')
-def create(title,body):
-    return {'title' : 'title', 'body' : 'body'}
+def create(request: Blog):
+    return request
 
