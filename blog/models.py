a87a362945077fa1b58082b40814069fab7960af
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-09-11 17:25:25 +0530

    Store blog into db

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from .database import Base
 
 
-class Blog(Base):
+class Blog(Base):         # Here Blog is actually extended base of database.
      __tablename__ = "blogs"
      id = Column(Integer, primary_key=True)
      title = Column(String)
