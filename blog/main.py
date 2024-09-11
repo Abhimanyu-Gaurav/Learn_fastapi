82a0f0165758c5cb48fb2ddb5513f4852a565d39
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-09-11 16:46:14 +0530

    Create Tables into Database

--- blog/main.py
+++ blog/main.py
@@ -1,10 +1,14 @@
  ## To perform CURD operation
 
 from fastapi import FastAPI
-from . import schemas
+from . import schemas, models
+from .database import engine
+
 
 app = FastAPI()
 
+
+models.Base.metadata.create_all(bind=engine)  # this will create Table in db.
 
 
 @app.post('/blog')
