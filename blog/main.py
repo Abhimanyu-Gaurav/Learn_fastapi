607ce6948284aeb64d647ec07bde86e0aa107d35
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-09-11 18:02:54 +0530

    Add Status code for our responses

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,6 @@
  ## To perform CURD operation
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -21,10 +21,21 @@ def get_db():
 
 
 
-@app.post('/blog')
+@app.post('/blog',status_code=status.HTTP_201_CREATED)  # Here status code of response is changed to 201 instead of 200, Because documented code for creation is 201.
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs 
+
+@app.get('/blog/{id}')
+def show(id, db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).filter(models.Blog.id ==id).first()
+    return blogs
