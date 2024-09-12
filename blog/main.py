bb8da9a2b82b61aee25b4964fd92fe162ff01485
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-09-12 11:31:16 +0530

    change in Delete method

--- blog/main.py
+++ blog/main.py
@@ -32,7 +32,12 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id==id).delete(synchronize_session=False)
+    blog= db.query(models.Blog).filter(models.Blog.id==id)
+
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id{id} nt found")
+
+    blog.delete(synchronize_session=False)
     db.commit()
     return "Done"
 
