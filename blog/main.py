fe2210f2b8b3ef6184021efa4076d73bb1838fe4
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-09-12 11:24:44 +0530

    Updated method

--- blog/main.py
+++ blog/main.py
@@ -37,6 +37,17 @@ def destroy(id, db: Session = Depends(get_db)):
     return "Done"
 
 
+@app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog,  db: Session = Depends(get_db) ):
+    blog = db.query(models.Blog).filter(models.Blog.id==id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id{id} nt found")
+    blog.update(request.dict())
+    db.commit()
+    return 'updated'
+
+
+
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
