c8777e7804da0dd89cddf5b11a522c0e49f67d8d
Abhimanyu Gaurav <kr.abhi.gaurav11@gmail.com>
2024-09-15 03:08:27 +0530

    verify password

--- blog/hashing.py
+++ blog/hashing.py
@@ -5,5 +5,9 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 class Hash():
     def bcrypt(password: str):
-        hashedPassword = pwd_context.hash('password')
+        hashedPassword = pwd_context.hash(password)
         return hashedPassword
+    
+    def verify(plain_password, hashed_password):
+        return pwd_context.verify(plain_password, hashed_password)
+
--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -2,6 +2,7 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from .. import schemas, database, models
 from sqlalchemy.orm import session
+from ..hashing import Hash
 
 router = APIRouter(tags= ['Authentication'])
 
@@ -11,4 +12,10 @@ def login(request: schemas.Login, db: session = Depends(database.get_db)):
     user = db.query(models.User).filter(models.User.email == request.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid Credentials")
+    
+    if not Hash.verify(request.password, user.password):
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Incorrect password")
+    
+    # generate JWT tokens and return
+
     return user
